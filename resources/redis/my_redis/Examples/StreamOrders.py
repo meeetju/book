d40b6366348690bf424e36fb1cec6cbf500be9ec
m.m <meeetju@gmail.com>
2025-01-21 23:51:39 +0100

    adhoc: redis

---
+++ resources/redis/my_redis/Examples/StreamOrders.py
@@ -0,0 +1,12 @@
+import csv
+import redis
+
+r = redis.Redis(password='redisPassword', host='localhost', port=6379)
+
+
+with open("OnlineRetail.csv", encoding='utf-8-sig') as csvf:
+	csvReader = csv.DictReader(csvf)
+
+	for row in csvReader:
+		print(row)
+		r.xadd("orders", row)
